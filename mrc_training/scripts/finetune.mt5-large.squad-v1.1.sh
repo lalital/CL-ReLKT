467a82429984343df404035a8c6cec8cc000dd6e
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2022-07-13 10:33:07 +0700

    Update params

--- mrc_training/scripts/finetune.mt5-large.squad-v1.1.sh
+++ mrc_training/scripts/finetune.mt5-large.squad-v1.1.sh
@@ -13,16 +13,18 @@ export WANDB_MODE=offline
 # BATCH_SIZES=(8)
 # LEARNING_RATES=(1.5e-5 1e-5)
 EPOCHS=(1 2 3 4 5)
-batch_size=${1}
+batch_size=16
+gradient_accumulation_steps=${1}
 learning_rate=${2}
-
+# gradient_accumulation_steps = 1 , bz = 16
+# gradient_accumulation_steps = 8 , bz = 128
 # for batch_size in "${BATCH_SIZES[@]}"
 # do
     # for learning_rate in "${LEARNING_RATES[@]}"
     # do
 for epoch in "${EPOCHS[@]}"
 do
-    echo "batch_size: ${batch_size}"
+    echo "batch_size: 16"
     echo "learning_rate: ${learning_rate}"
     echo "epoch: ${epoch}"
 
@@ -39,16 +41,17 @@ do
     --evaluation_strategy epoch \
     --save_strategy epoch \
     --warmup_ratio 0.06 \
-    --per_device_train_batch_size ${batch_size} \
+    --per_device_train_batch_size 16 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps ${gradient_accumulation_steps}
     --load_best_model_at_end True \
-    --save_total_limit 1 \
     --fp16 True \
     --learning_rate ${learning_rate} \
     --num_train_epochs ${epoch} \
     --max_seq_length 512 \
     --doc_stride 128 \
-    --max_answer_length 50 \
-    --generation_max_length 50 \
+    --max_answer_length 30 \
+    --generation_max_length 30 \
     --output_dir ./checkpoints/${run_name} \
     --logging_dir ./logs/${run_name} |& tee -a ./logs/${run_name}/trainer.log
 done
