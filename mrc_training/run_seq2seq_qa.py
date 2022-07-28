2adf893b5c2b6d0f4536cfd4457fc5e7a0f5ef3f
Lalita Lowphansirikul <lalita.lowphansirikul@gmail.com>
2022-07-28 17:01:30 +0700

    Add first step eval

--- mrc_training/run_seq2seq_qa.py
+++ mrc_training/run_seq2seq_qa.py
@@ -662,6 +662,9 @@ def main():
             checkpoint = training_args.resume_from_checkpoint
         elif last_checkpoint is not None:
             checkpoint = last_checkpoint
+        # test first step eval
+        eval_result = trainer.evaluate()
+        
         train_result = trainer.train(resume_from_checkpoint=checkpoint)
         trainer.save_model()  # Saves the tokenizer too for easy upload
 
